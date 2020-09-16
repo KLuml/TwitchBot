1a1bc4796cc5f40834afcaf7ecf1a0feae54b5db
KLuml <55952460+KLuml@users.noreply.github.com>
2020-09-15 20:57:40 -0400

    Update ClientInformation.java

--- src/ClientInformation.java
+++ src/ClientInformation.java
@@ -4,9 +4,9 @@ public class ClientInformation {
 
     //Put your Bot's Info here.
     //You will need to make a twitch account and apply for an oauth code from twitch.
-    public final String NICK = "imarealbotnow";
-    public final String PASS = "oauth:7rgcb2446o3dxeevszcnw33jb45g3v";
-    public final String channel = "wekeepsitreal";  //twitch channel you'd like to visit.
+    public final String NICK = "";
+    public final String PASS = "oauth:";
+    public final String channel = "";  //twitch channel you'd like to visit.
 
 }
 // Password / oauth / NICK / channel / User
